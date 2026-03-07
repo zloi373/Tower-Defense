a5db90ddb9a0b678c521130905c6feddc2218b15
dpetuhov1 <xzxz228n8@gmail.com>
2026-03-07 16:43:38 +0300

    Добавили класс Sprite и создали игрока

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import pygame
 
+class Sprite:
+    def __init__(self, image, center):
+        self.image = image
+        self.rect = self.image.get_frect()
+        self.rect.center = center
+    
+    def render(self):
+        surface.blit(self.image, self.rect)
+
+
+
 
 WINDOW_SIZE = [800,600]
 MAX_FPS = 60
 
-window = pygame.window('Tower Defense', WINDOW_SIZE)
+window = pygame.Window('Tower Defense', WINDOW_SIZE)
 surface = window.get_surface()
 clock = pygame.Clock()
+
+image = pygame.Surface([50,50])
+image.fill("red")
+player = Sprite(image, [WINDOW_SIZE[0] / 2, WINDOW_SIZE[1] / 2])
+
+
+
 running = True
 while running:
     #Обработка событий
@@ -19,7 +37,7 @@ while running:
 
     #Отрисовка
     surface.fill('black')
-    
+    player.render()
     window.flip()
 
     clock.tick(MAX_FPS)
